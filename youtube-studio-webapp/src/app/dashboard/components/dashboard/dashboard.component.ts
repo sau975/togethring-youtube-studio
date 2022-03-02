d243b8010c28c48f897cbf4d5dd815faf4232939
Saurav Kumar <saurav97531@gmail.com>
2022-03-03 01:16:22 +0530

    dashboard in progress

--- youtube-studio-webapp/src/app/dashboard/components/dashboard/dashboard.component.ts
+++ youtube-studio-webapp/src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -9,12 +9,29 @@ import { AuthService } from 'src/app/login/services/auth.service';
 })
 export class DashboardComponent implements OnInit {
 
+  displaySideBar: boolean = true;
+  userDetails: any;
+
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
+    let user:any = localStorage.getItem('user');
+    this.userDetails = JSON.parse(user);
+  }
+
+  onSelectFile(event:any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      // reader.onload = (event) => { // called once readAsDataURL is completed
+      //   this.url = event.target.result;
+      // }
+    }
   }
 
   logout() {
--- youtube-studio-webapp/src/app/shared/shared.module.ts
+++ youtube-studio-webapp/src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {SidebarModule} from 'primeng/sidebar';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SidebarModule
   ],
   exports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SidebarModule
   ]
 })
 export class SharedModule { }
