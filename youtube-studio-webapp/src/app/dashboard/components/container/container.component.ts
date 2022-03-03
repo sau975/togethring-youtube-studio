8d0ea46f283f30122ed32cdef53ce959eb0d9fe0
Saurav Kumar <saurav97531@gmail.com>
2022-03-04 01:35:11 +0530

    container and content component added

---
+++ youtube-studio-webapp/src/app/dashboard/components/container/container.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/login/services/auth.service';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css']
+})
+export class ContainerComponent implements OnInit {
+
+  displaySideBar: boolean = true;
+  userDetails: any;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    let user:any = localStorage.getItem('user');
+    this.userDetails = JSON.parse(user);
+  }
+
+  onSelectFile(event:any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      // reader.onload = (event) => { // called once readAsDataURL is completed
+      //   this.url = event.target.result;
+      // }
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+}
