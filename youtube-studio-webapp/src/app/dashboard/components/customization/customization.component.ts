6d18115bb9c702b5835b736c0e896888d09c68dc
Saurav Kumar <saurav97531@gmail.com>
2022-03-06 23:07:58 +0530

    drag drop implemented

--- youtube-studio-webapp/src/app/dashboard/components/customization/customization.component.ts
+++ youtube-studio-webapp/src/app/dashboard/components/customization/customization.component.ts
@@ -3,15 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-customization',
   templateUrl: './customization.component.html',
-  styleUrls: ['./customization.component.css']
+  styleUrls: ['./customization.component.scss']
 })
 export class CustomizationComponent implements OnInit {
 
-  lists = [
-    { id: 1, name: 'Code' },
-    { id: 2, name: 'Name' },
-    { id: 3, name: 'Category' },
-    { id: 4, name: 'Quantity' }
+  products: IProduct[] = [
+    { id: 1, name: 'Book', image: "", price: 12, category: "Stationary", inventoryStatus: "Available"},
+    { id: 2, name: 'Laptop', image: "", price: 15, category: "Electronic", inventoryStatus: "Available"},
+    { id: 3, name: 'Mobile', image: "", price: 20, category: "Electronic", inventoryStatus: "Available"},
+    { id: 4, name: 'Pen', image: "", price: 25, category: "Stationary", inventoryStatus: "Available"}
   ];
 
   constructor() { }
@@ -21,4 +21,14 @@ export class CustomizationComponent implements OnInit {
   onReorder(value:any){
     console.log(value);
   }
+}
+
+export interface IProduct
+{
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  category: string;
+  inventoryStatus: string;
 }
